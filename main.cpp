1a7ee4694ad8cdfe5e2bbac3c03f87807b676e18
yonazone <ubuacc@gmail.com>
2024-09-15 15:40:16 +0530

    made a function to count no. of files in the directory

--- main.cpp
+++ main.cpp
@@ -9,18 +9,35 @@
 #include <condition_variable>
 #include <mutex>
 #include <unordered_map>
+#include <format>
 
 class DirWatcher
 {
 private:
   const std::string& pathToWatch;
+  //std::filesystem::path& file;
+  std::vector<const std::filesystem::path&> mfile;
 
 public: // CONSTRUCTORS
   DirWatcher(const std::string& _pathToWatch) // maybe add one more, time to monitor (chrono)
     : pathToWatch(_pathToWatch)
   {
     // Maybe display all the files with their corresponding extension(type), date created, last modified and all these details
+    for (std::size_t i = 0u; i < number_of_files_in_directory(pathToWatch); ++i)
+    {
+      
+    }
   }
 
 public: // MEMBER FUNCTIONS
+  std::size_t number_of_files_in_directory(std::filesystem::path path)
+  {
+    std::size_t number_of_files = 0u;
+    for (auto const& file : std::filesystem::directory_iterator(path))
+    {
+      mfile.push_back(file);
+      ++number_of_files;
+    }
+    return number_of_files;
+  }
 };
